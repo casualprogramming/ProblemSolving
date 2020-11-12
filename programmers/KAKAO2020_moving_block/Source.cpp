50f22fb248979b3d26df88cabd43a860717da159
yechan <yechan@skku.edu>
2020-11-12 19:54:59 +0900

    KAKAO 2020 moving block complete

--- programmers/KAKAO2020_moving_block/Source.cpp
+++ programmers/KAKAO2020_moving_block/Source.cpp
@@ -1,85 +1,111 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 template <typename T, typename U>
 std::pair<T, U> operator+(const std::pair<T, U>& l, const std::pair<T, U>& r) {return { l.first + r.first,l.second + r.second };}
 template <typename T, typename U>
-std::pair<T, U> operator-(const std::pair<T, U>& l, const std::pair<T, U>& r) {return { l.first + r.first,l.second + r.second };}
+std::pair<T, U> operator-(const std::pair<T, U>& l, const std::pair<T, U>& r) {return { l.first - r.first,l.second - r.second };}
+
+typedef typename pair<int, int> XY;
+int minimalStep[2][102][102];
+int myboard[102][102];
+int n = 0;
 enum STATE
 {
 	HORIZONTAL,VERTICAL
 };
+
+bool isMovable(XY src)
+{
+	if (src.second < 0 || src.first < 0)
+		printf("warning\n");
+	return myboard[src.second][src.first]== 0;
+}
+
+bool isRotatable(XY source0, XY move_source1, XY dst)
+{
+	if (!isMovable(dst))
+		return false;
+	XY check = source0 + (move_source1-source0) + (dst-source0);
+	if (!isMovable(check))
+		return false;
+	return true;
+}
+
+bool isMovable(XY src0, XY src1, XY dir)
+{
+	src0 = src0 + dir;
+	src1 = src1 + dir;
+	return isMovable(src0) && isMovable(src1);
+}
+
 struct robotState
 {
-	pair<int, int> src[2];
+	XY src;
 	STATE state;
-	vector<pair<int, int>> getDirections()
+	vector<robotState> getNextStates()
 	{
+		vector<robotState> states;
+
+		vector<XY> dirs = { {0,1},{0,-1},{1,0},{-1,0}};
+		for (auto dir : dirs)
+		{
+			XY src0 = (state == HORIZONTAL) ? src - XY(1, 0) : src - XY(0, 1);
+			if (isMovable(src0, src, dir))
+				states.push_back({ src + dir , state});
+		}
+
 		if (state == HORIZONTAL)
-	{
-		vector<robotState> directions;
-		//left-down
-		if (isRotatable(src[0], src[1], src[0] + pair<int,int>{0,1}))
-			directions.push_back({ src[0] ,src[0] + pair<int,int>{0,1}});
-		//left-up
-		if(isRotatable(src[0], src[1], {x-1,y-1}))
-			directions.push_back({x-1,y-1});
-		//right-down
-		if(isRotatable({x,y}, {x-1,y}, {x,y+1}))
-			directions.push_back({x,y+1});
-		//right-up
-		if(isRotatable({x,y}, {x-1,y}, {x,y-1}))
-			directions.push_back({x,y-1});
-		return directions;
-	}
-	
-	//VERTICAL
-	else
-	{
-		vector<pair<int, int>> directions;
-		//left-up
-		if(isRotatable({x,y-1}, {x,y}, {x-1,y-1}))
-			directions.push_back({x-1,y-1});
-		//right-up
-		if(isRotatable({x,y-1}, {x,y}, {x+1,y-1}))
-			directions.push_back({x+1,y-1});
-		//left-down
-		if(isRotatable({x,y}, {x,y-1}, {x-1,y}))
-			directions.push_back({x-1,y});
-		//right-down
-		if(isRotatable({x,y}, {x,y-1}, {x+1,y}))
-			directions.push_back({x+1,y});
-		return directions;
-	}
+		{
+			XY src0 = { src - XY(1,0) };
+			//left-down
+			if (isRotatable(src0, src,	src0 + XY(0,1)))
+				states.push_back({	src0 + XY(0,1),  VERTICAL});
+			//left-up
+			if (isRotatable(src0, src,	src0 + XY(0,-1)))
+				states.push_back({	src0, VERTICAL});
+			//right-down
+			if (isRotatable(src, src0,	src	+ XY(0,1)))
+				states.push_back({	src + XY(0,1) ,	 VERTICAL});
+			//right-up
+			if (isRotatable(src, src0,	src + XY(0,-1)))
+				states.push_back({	src ,  VERTICAL});
+		}
+
+		//VERTICAL
+		else
+		{
+			XY src0 = { src - XY(0,1) };
+			//right-up
+			if (isRotatable(src0, src, src0 + XY(1,0)))
+				states.push_back({ src0 + XY(1,0),  HORIZONTAL });
+			//left-up
+			if (isRotatable(src0, src, src0 + XY(-1,0)))
+				states.push_back({ src0 , HORIZONTAL });
+			//right-down
+			if (isRotatable(src, src0, src + XY(1,0)))
+				states.push_back({ src + XY(1,0) ,	HORIZONTAL });
+			//right-up
+			if (isRotatable(src, src0, src + XY(-1,0)))
+				states.push_back({ src ,  HORIZONTAL });
+		}
+		return states;
 	}
 
 };
 
-int minimalStep[2][102][102];
-int myboard[102][102];
-int n = 0;
 
-bool isRotatable(pair<int, int> source0, pair<int, int> move_source1, pair<int, int> dst)
+
+void move(robotState s, int step)
 {
-	if (myboard[dst.first][dst.second] == 1)
-		return false;
-	pair<int, int> check = source0 + (source0 - move_source1) + (source0 - dst);
-	if (myboard[check.first][check.second] == 1)
-		return false;
-	return true;
-}
-vector<pair<int, int>> getDirections(STATE state, int x, int y)
-{
-	
-}
-void move(STATE state, int x, int y, int step)
-{
-	if (minimalStep[state][x][y] <= step)
+	if (minimalStep[s.state][s.src.second][s.src.first] <= step)
 		return;
-	minimalStep[state][x][y] = step;
-	for (auto const& dir : getDirections(state, x, y))
+	printf("state: %s %d %d step:%d\n", s.state==VERTICAL?"VERTICAL":"HORIZON", s.src.first, s.src.second, step);
+	minimalStep[s.state][s.src.second][s.src.first] = step;
+	for (auto const &next_state : s.getNextStates())
 	{
-		move(state == HORIZONTAL ? VERTICAL : HORIZONTAL, dir.first, dir.second, step + 1);
+		move(next_state, step+1);
 	}
 }
 
@@ -89,18 +115,22 @@ int solution(vector<vector<int>> board) {
 	for(int i=0;i<n+2;i++)
 		for (int j=0;j<n+2;j++)
 			myboard[i][j]=1;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
+	for (int i = 0; i < n+2; i++)
+		for (int j = 0; j < n+2; j++)
 			for(int s=0;s<2;s++)
 				minimalStep[s][i + 1][j + 1] =INT_MAX;
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < n; j++)
 			myboard[i+1][j+1]=board[i][j];
-	move(HORIZONTAL, 2, 1, 0);
-	return answer;
+	move({ {2, 1}, HORIZONTAL}, 0);
+	return min(minimalStep[0][n][n], minimalStep[1][n][n]);
 }
 void main()
 {
 	vector<vector<int>> input = { {0, 0, 0, 1, 1}, {0, 0, 0, 1, 0}, {0, 1, 0, 1, 1}, {1, 1, 0, 0, 1}, {0, 0, 0, 0, 0} };
-	solution(input);
+	cout<<solution(input)<<endl;
 }
+
+
+//TODO : Don't find the next possible state, go ahead and check if that current state is correct or not
+//that will be simpler code
