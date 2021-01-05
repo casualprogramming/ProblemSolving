16c7884e5ec787e8f849bbfe680abbfdb26ce801
casualprogramming <syc9624@gmail.com>
2021-01-05 18:03:58 +0900

    simplify binary search level3 immigration

--- programmers/src/binarySearch_leve3_immigration/Source.cpp
+++ programmers/src/binarySearch_leve3_immigration/Source.cpp
@@ -6,31 +6,15 @@
 
 //@@Make sure to check long long type
 
+//--final simple version
 using namespace std;
-enum { LESS, EQUAL, GREATER };
-long long getCompactTime(long long t, vector<int> const& times, long long n)
-{
-	long long remainTimes = LLONG_MAX;
-	for (auto time : times)
-	{
-		remainTimes = min(remainTimes, t % time);
-	}
-	return t - remainTimes;
-}
-
-long long getState(long long t, vector<int> const& times, long long n)
+long long isEnough(long long t, vector<int> const& times, long long n)
 {
 	long long processed = 0;
 	for (auto time : times)
-	{
 		processed += t / time;
-	}
-	if (processed > n)
-		return GREATER;
-	else if (processed == n)
-		return EQUAL;
-	else
-		return LESS;
+
+	return processed >= n ? true : false;
 }
 
 long long solution(int n, vector<int> times) {
@@ -42,23 +26,12 @@ long long solution(int n, vector<int> times) {
 		long long processed = 0;
 		long long t = (r - l) / 2 + l;
 
-		auto type = getState(t, times, n);
-		if (type == GREATER)
-		{
+		if (isEnough(t, times, n))
 			r = t;
-		}
-		else if (type == LESS)
-		{
+		else
 			l = t;
-		}
-		else//EQUAL
-		{
-			answer = getCompactTime(t, times, n);
-			break;
-		}
 	}
-	if (answer == 0)
-		answer = r;
+	answer = r;
 	return answer;
 }
 
@@ -66,3 +39,60 @@ void main()
 {
 	cout << solution(6, {7,10}) << endl;
 }
+
+//--old dirty version: 3 type of return value
+//--difference: eliminates wasted time when equal
+//enum { LESS, EQUAL, GREATER };
+//long long getCompactTime(long long t, vector<int> const& times, long long n)
+//{
+//	long long remainTimes = LLONG_MAX;
+//	for (auto time : times)
+//	{
+//		remainTimes = min(remainTimes, t % time);
+//	}
+//	return t - remainTimes;
+//}
+//
+//long long getState(long long t, vector<int> const& times, long long n)
+//{
+//	long long processed = 0;
+//	for (auto time : times)
+//	{
+//		processed += t / time;
+//	}
+//	if (processed > n)
+//		return GREATER;
+//	else if (processed == n)
+//		return EQUAL;
+//	else
+//		return LESS;
+//}
+//
+//long long solution(int n, vector<int> times) {
+//	long long answer = 0;
+//	sort(times.begin(), times.end());
+//	long long l = 0, r = n * times.front();
+//	while (r - l != 1)
+//	{
+//		long long processed = 0;
+//		long long t = (r - l) / 2 + l;
+//
+//		auto type = getState(t, times, n);
+//		if (type == GREATER)
+//		{
+//			r = t;
+//		}
+//		else if (type == LESS)
+//		{
+//			l = t;
+//		}
+//		else//EQUAL
+//		{
+//			answer = getCompactTime(t, times, n);
+//			break;
+//		}
+//	}
+//	if (answer == 0)
+//		answer = r;
+//	return answer;
+//}
