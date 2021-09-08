fd4fba7aa5915af99982c2a74b82cacb626eac13
casualprogramming <syc9624@gmail.com>
2021-09-08 20:01:58 +0900

    Create programmers_level2_sorting_H_index.py

---
+++ programmers/src_python/programmers/programmers_level2_sorting_H_index.py
@@ -0,0 +1,23 @@
+from itertools import permutations, combinations, product, combinations_with_replacement
+import sys       
+import math
+from collections import defaultdict, deque, OrderedDict
+from enum import Enum
+from pprint import pprint
+from bisect import bisect_left, bisect_right
+
+def solution(citations):
+    answer = 0
+    citations.sort()
+    Min = citations[0]
+    Max = citations[len(citations)-1]
+    for h in range(Min, Max+1):
+        h_more_papers = len(citations) - bisect_left(citations, h)
+        if h_more_papers>=h:
+            answer = max(answer, h)
+    return answer
+
+
+print(solution([3, 0, 6, 1, 5]))
+print(solution([12,11,10,9,8,1]))
+print(solution([6,6,6,6,6,1]))
