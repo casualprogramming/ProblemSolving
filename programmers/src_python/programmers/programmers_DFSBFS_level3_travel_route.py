3e08a1f3a16987a65b032af24c4a315bcf7744c9
casualprogramming <syc9624@gmail.com>
2021-09-12 01:52:03 +0900

    clean up

--- programmers/src_python/programmers/programmers_DFSBFS_level3_travel_route.py
+++ programmers/src_python/programmers/programmers_DFSBFS_level3_travel_route.py
@@ -2,44 +2,6 @@ from itertools import permutations, combinations, product, combinations_with_rep
 from collections import deque,defaultdict
 import sys       
 
-def IsConvertable(str1,str2):
-    if len(str1) != len(str2):
-        return False
-    count=0
-    for c1,c2 in zip(str1,str2):
-        if c1!=c2:
-            count+=1
-    if count==1:
-        return True
-    return False
-
-#post-check
-def bfs(begin_word, target_word, words):
-    queue = deque()
-    queue.append((begin_word,0))
-    while queue:
-        word, level = queue.popleft()
-        if word == target_word:
-            return level
-        if level==len(words)-1:
-            continue
-        next_words = map(lambda x: (x,level+1),  filter(lambda x: IsConvertable(word,x),words))
-        queue.extend(next_words)
-    return 0
-
-#
-def dfs(index, graph, remains:set):
-    stack = list()
-    stack.append(index)
-    dfs()
-    while stack:
-        vertex = stack.pop()
-        if vertex in remains:
-            remains.remove(vertex)
-            #OnVisited(vertex)
-            stack.extend(graph[vertex])
-
-
 ticket_size = 0
 FIND = False
 def dfs(begin, graph:dict, route:list):
